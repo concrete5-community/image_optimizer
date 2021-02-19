1f23f67d61d4de6e3c50bc93e6e6dd99a2595927
a3020 <a3020@fastmail.com>
2021-02-19 18:26:45 +0100

    Version 2.0.9

--- elements/dashboard/install_post.php
+++ elements/dashboard/install_post.php
@@ -1,26 +1,27 @@
 <?php
+
 defined('C5_EXECUTE') or die('Access Denied.');
 
 use Concrete\Core\Support\Facade\Url;
 ?>
+
 <p><?php echo t("Congratulations, %s has been installed!", t('Image Optimizer')); ?></p>
 <br>
 
 <p>
-    <strong><?php echo t('In order to configure it go to:'); ?></strong>
+    <strong><?php echo t('Configure %s:', t('Image Optimizer')); ?></strong>
     <a class="btn btn-default" href="<?php echo Url::to('/dashboard/system/files/image_optimizer') ?>">
         <?php
-        echo t('System & Settings / Files / Image Optimizer.');
+        echo t('System & Settings / Files / Image Optimizer');
         ?>
     </a>
 </p>
 <br>
 
-<p>
-    <strong><?php echo t('In order to run it go to:'); ?></strong>
-    <a class="btn btn-default" href="<?php echo Url::to('/dashboard/system/optimization/jobs') ?>">
-        <?php
-        echo t('System & Settings / Optimization / Automated Jobs.');
-        ?>
-    </a>
+<p class="alert alert-warning">
+    <?php
+    echo t("Tip: Make sure you install the optimizers on your server or configure the TinyPNG service before running the Automated Job. See also the %sinstallation instructions%s.",
+        '<a href="https://www.concrete5.org/marketplace/addons/image-optimizer/installation/" target="_blank">',
+        '</a>');
+    ?>
 </p>
