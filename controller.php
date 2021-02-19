6f2a9562065512915742ec2b8c600c2a6ef81f15
a3020 <a3020@fastmail.com>
2021-02-19 18:26:35 +0100

    Version 1.4.2

--- controller.php
+++ controller.php
@@ -18,7 +18,7 @@ final class Controller extends Package
 {
     protected $pkgHandle = 'image_optimizer';
     protected $appVersionRequired = '8.0';
-    protected $pkgVersion = '1.4.1';
+    protected $pkgVersion = '1.4.2';
     protected $pkgAutoloaderRegistries = [
         'src/ImageOptimizer' => '\A3020\ImageOptimizer',
     ];
